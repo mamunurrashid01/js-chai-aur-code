541854e88bcd90287550967a41619564be144b02
mamunurrashid01 <138356173+mamunurrashid01@users.noreply.github.com>
2025-05-10 11:58:09 +0000

    data type interview question discusion

---
+++ js_basic/04_operation.js
@@ -0,0 +1,19 @@
+let fname = " Mamunur"
+let lname = " Rashid"
+let num1 = 10
+let num2 = 15
+// console.log(fname + lname)
+// console.log(typeof (num1 + fname) )
+
+let join = "1" + 2+ 2
+let join1 = 1 + 2 + Number("2")
+// console.log(join)
+// console.log(join1)
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0)
+console.log(undefined > 0)
+console.log(undefined == 0)
+console.log(undefined < 0)
+console.log("2" > 1)
+console.log("02" > 1)

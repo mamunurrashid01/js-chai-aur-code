1b3decae2d0b0ae20482ecc2fc7d2ddce7040ad9
mamunurrashid01 <138356173+mamunurrashid01@users.noreply.github.com>
2025-05-10 04:39:25 +0000

    data type and data conversion

--- js_basic/02_datatype.js
+++ js_basic/02_datatype.js
@@ -7,3 +7,9 @@
 6.undefine => undefine type data
 7.symbole => unique data type
 */
+let name = "Mamunur"
+let age = 36
+let isLogged = true
+let orio = null
+let averest;
+console.table(typeof orio)
